d9edffc663d00326990c80c1417416d87d447b9f
gus <admac231@gmail.com>
2022-03-19 09:06:36 +0000

    Repalced some auto-generated templates and added the web-api service

--- web/ayedroid.poker.ui/src/app/app.module.ts
+++ web/ayedroid.poker.ui/src/app/app.module.ts
@@ -4,6 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { WebApiService } from './services/web-api.service';
+import { HttpClientModule } from '@angular/common/http';
+import {MatInputModule} from '@angular/material/input';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [
@@ -12,9 +22,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    HttpClientModule,
+    MatInputModule,
+    MatTooltipModule
   ],
-  providers: [],
+  providers: [WebApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ web/ayedroid.poker.ui/src/app/services/web-api.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebApiService {
+  private baseUri = 'https://localhost:7241';
+
+  constructor(private http: HttpClient) { }
+
+  public newUser(userName: string): Observable<string> {
+    return this.http.post<string>(`${this.baseUri}/User`, { UserName: userName } );
+  }
+}
