e9115a27627205e44e37acd05795fe468e95a89b
gus <admac231@gmail.com>
2022-03-19 09:48:31 +0000

    Added responsive nav bar

--- web/ayedroid.poker.ui/src/app/app.module.ts
+++ web/ayedroid.poker.ui/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { WebApiService } from './services/web-api.service';
 import { HttpClientModule } from '@angular/common/http';
 import {MatInputModule} from '@angular/material/input';
 import {MatTooltipModule} from '@angular/material/tooltip';
+import {FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import {MatTooltipModule} from '@angular/material/tooltip';
     MatListModule,
     HttpClientModule,
     MatInputModule,
-    MatTooltipModule
+    MatTooltipModule,
+    FlexLayoutModule  
   ],
   providers: [WebApiService],
   bootstrap: [AppComponent]
