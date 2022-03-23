5dda4f830e1f557795ec6d51cb7a64ebb3b02df8
gus <admac231@gmail.com>
2022-03-23 22:58:48 +0000

    Added start session feature to UI

--- web/ayedroid.poker.ui/src/app/app.component.ts
+++ web/ayedroid.poker.ui/src/app/app.component.ts
@@ -9,29 +9,5 @@ import { WebApiService } from './services/web-api.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  public newSessionLoading: boolean = false;
-  constructor(
-    private webApiService: WebApiService,
-    private snackBar: MatSnackBar,
-    private translocoService: TranslocoService
-  ) {}
-
-  public startSession(): void {
-    this.newSessionLoading = true;
-
-    this.webApiService
-      .startSession('test')
-      .subscribe({
-        error: () =>
-          this.snackBar
-            .open(
-              this.translocoService.translate('ERRORS.CREATE SESSION'),
-              this.translocoService.translate('MAIN.RETRY')
-            )
-            .onAction()
-            .subscribe(() => this.startSession()),
-        complete: () => console.info('complete'),
-      })
-      .add(() => (this.newSessionLoading = false));
-  }
+  constructor() {}
 }
--- web/ayedroid.poker.ui/src/app/home/home.component.ts
+++ web/ayedroid.poker.ui/src/app/home/home.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { TranslocoService } from '@ngneat/transloco';
+import { WebApiService } from '../services/web-api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  public newSessionLoading: boolean = false;
 
-  constructor() { }
+  constructor(
+    private webApiService: WebApiService,
+    private snackBar: MatSnackBar,
+    private translocoService: TranslocoService,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  public startSession(sessionName: string): void {
+    this.newSessionLoading = true;
+
+    this.webApiService
+      .startSession(sessionName)
+      .subscribe({
+        next: (sessionId) => this.router.navigate(['/', sessionId]),
+        error: () => {
+          this.snackBar
+            .open(
+              this.translocoService.translate('ERRORS.CREATE SESSION'),
+              this.translocoService.translate('MAIN.RETRY')
+            )
+            .onAction()
+            .subscribe(() => this.startSession(sessionName));
+        },
+      })
+      .add(() => (this.newSessionLoading = false));
   }
-
 }
