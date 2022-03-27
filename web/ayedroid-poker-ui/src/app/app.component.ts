f94a16e96a823ab0f7e44c4bbfff9f5d3773563c
gus <admac231@gmail.com>
2022-03-27 02:12:33 +0100

    Added copy button to copy session URL

--- web/ayedroid-poker-ui/src/app/app.component.ts
+++ web/ayedroid-poker-ui/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { UserStorageService } from './services/user-storage.service';
+import { Clipboard } from '@angular/cdk/clipboard';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { TranslocoService } from '@ngneat/transloco';
 
 @Component({
   selector: 'pkr-root',
@@ -7,5 +10,38 @@ import { UserStorageService } from './services/user-storage.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  public constructor(public userStorageService: UserStorageService) {}
+  public constructor(
+    public userStorageService: UserStorageService,
+    public clipboard: Clipboard,
+    private snackBar: MatSnackBar,
+    private translocoService: TranslocoService
+  ) {}
+
+  public copyUrl(): void {
+    this.copyToClipboard(window.location.href);
+  }
+
+  private copyToClipboard(str: string): void {
+    const pending = this.clipboard.beginCopy(str);
+
+    let remainingAttempts = 3;
+    const attempt = (): void => {
+      const result = pending.copy();
+      if (!result && --remainingAttempts) {
+        setTimeout(attempt);
+      } else {
+        // Remember to destroy when you're done!
+        this.snackBar.open(
+          this.translocoService.translate(
+            'MAIN.{{ENTITY}} COPIED TO CLIPBOARD',
+            {
+              ENTITY: str,
+            }
+          )
+        );
+        pending.destroy();
+      }
+    };
+    attempt();
+  }
 }
